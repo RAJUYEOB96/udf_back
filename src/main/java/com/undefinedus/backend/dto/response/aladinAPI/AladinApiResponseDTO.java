22300f69ef3dfa76e63846e6af68a4387b441f86
kimYong <rladyd97@naver.com>
2024-11-15 11:13:38 +0900

    fix(aladinBook): 알라딘 도서 api에 맞게 업데이트
    알라딘 도서 api에 맞는 형태로 코드 업데이트
    - 바뀐 entity의 내용에 따라 영향을 받는 다른 코드들 업데이트
    - dto 추가 및 필요한 필드만 가져오도록 업데이트

---
+++ src/main/java/com/undefinedus/backend/dto/response/aladinAPI/AladinApiResponseDTO.java
@@ -0,0 +1,30 @@
+package com.undefinedus.backend.dto.response.aladinAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class AladinApiResponseDTO {
+    private String title;
+    private String link;
+    private String author;
+    private String isbn13;
+    private String cover;
+    private String categoryName;
+    private String publisher;
+    private Boolean adult;
+    private Double customerReviewRank;
+    private String fullDescription;
+
+    private SubInfoDTO subInfo;
+
+    // 나중에 있는지 없는지 확인해서 넣어줄 예정
+    private String status;
+}
---
+++ src/main/java/com/undefinedus/backend/dto/response/aladinAPI/SubInfoDTO.java
@@ -0,0 +1,16 @@
+package com.undefinedus.backend.dto.response.aladinAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class SubInfoDTO {
+    private Integer itemPage;
+}
