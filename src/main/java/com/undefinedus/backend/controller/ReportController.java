e045f36565157d584b666d16ece83b08fb47cfa0
Jaewon94 <wodnjs3580@gmail.com>
2024-12-16 18:51:45 +0900

    fix(discussionComment): 베스트 3 토론 댓글에 로그인 사용자가 신고했는지에 대한 필드 업데이트
    - discussionCommentService.getBest3CommentByCommentLikes 진행시 체크후 isReport 값 넣어주는 코드 업데이트

--- src/main/java/com/undefinedus/backend/controller/ReportController.java
+++ src/main/java/com/undefinedus/backend/controller/ReportController.java
@@ -35,9 +35,7 @@ public class ReportController {
     ) {
 
         Long memberId = memberSecurityDTO.getId();
-
-        // TODO : 신고를 두번 할 수 없음, 그래서 ResponseDTO에 isReport(boolean)을 만들어서 넣어줘야 할듯
-        // 그래서 프론트에서 true면 신고하기가 안보이고, false면 신고하기 보이는 방식으로
+        
         reportService.reportDiscussion(memberId, discussionId, reportRequestDTO);
 
         return ResponseEntity.status(HttpStatus.OK)
